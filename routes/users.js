39415b7d62fdcc300b041f9e79a6f486f845075f
Nachiketa Pathak <70041514+nachiketap11@users.noreply.github.com>
2026-04-29 10:53:48 -0400

    Adding code for authentication (#3)
    * Added files req for auth
    
    * Updated readme to include docs
    
    * delete file
    
    * adding gitignore

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
+const async = require('hbs/lib/async');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/id',async function(req,res,next){
+  res.render('id', {idTokenClaims:req.session.account.idTokenClaims})
+})
 
 module.exports = router;
